9c52d4b2175661eada73076e1bb9f1573fc8bbdd
Gunduz Heydarov <g.heyderov@gmail.com>
2025-01-10 19:56:04 +0400

    django-admin

--- product/admin.py
+++ product/admin.py
@@ -4,7 +4,39 @@ from .models import ProductCategory, Product, ProductImage, ProductTag, ProductR
 # Register your models here.
 
 admin.site.register(ProductCategory)
-admin.site.register(Product)
-admin.site.register(ProductImage)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+
+class ProductReviewInline(admin.TabularInline):
+    model = ProductReview
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'category', 'price', 'get_tags']
+    list_display_links = ['title']
+    list_editable = ('category',)
+    list_filter = ('price', 'category')
+    list_per_page = 2
+    search_fields = ('title', 'category__title')
+    inlines = [ProductImageInline, ProductReviewInline]
+
+    def get_tags(self, obj):
+        tags = []
+        for tag in obj.tags.all():
+            tags.append(tag.title)
+        return tags
+
+
 admin.site.register(ProductTag)
-admin.site.register(ProductReview)
+
+@admin.register(ProductReview)
+class ProductReviewAdmin(admin.ModelAdmin):
+    list_display = ['product', 'message', 'is_verified']
+    actions = ['update_status']
+
+    def update_status(self, request, queryset):
+        queryset.update(is_verified = True)
